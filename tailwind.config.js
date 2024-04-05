2c9d71349276edbc2492a9226e4fa63a8eaa986d
Matt P <mpoteshman1@gmail.com>
2024-04-04 22:12:01 -0500

    champs updates 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,21 @@ module.exports = {
           "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
           "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
           "--border-btn": "2px", // border width of buttons
+        },
+        bw: {
+          "primary": "#000",
+          "secondary": "#000",
+          "neutral": "#fff",
+          'base-100': '#fff',
+          'accent': '#ddd',
+          'error': '#000',
+          "--rounded-box": "0.25rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.25rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.125s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "2px", // border width of buttons
         }
       }
     ]
